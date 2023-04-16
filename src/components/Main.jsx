7db09c892b30a6c49cd15548ebdc5164190ffcc4
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-16 21:18:13 +0900

    srcディレクトリ配下にリソースを移動

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import styles from "./Main.module.css";
+import { Links } from "@/src/components/Links";
+import { HeadLine } from "@/src/components/HeadLine";
+
+export function Main(props) {
+  return (
+    <main className={styles.main}>
+      <HeadLine page={props.page}>
+        {<code className={styles.code}>pages/{props.page}.js</code>}
+      </HeadLine>
+      <Links />
+    </main>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import { Footer } from "@/components/Footer";
-import { Main } from "@/components/Main";
-import { Header } from "@/components/Header";
+import { Footer } from "@/src/components/Footer";
+import { Main } from "@/src/components/Main";
+import { Header } from "@/src/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
