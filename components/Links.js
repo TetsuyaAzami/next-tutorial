f35b0821a8a71cbbde33aca8015ec7c5046833ff
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-16 18:16:41 +0900

    繰り返し処理をリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -4,64 +4,46 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export function Links() {
+	const ITEMS = [
+		{
+			href: 'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+			title: 'Docs',
+			description: 'Find in-depth information about Next.js features and&nbsp;API.'
+		},
+		{
+			href: 'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+			title: 'Learn',
+			description: 'Learn about Next.js in an interactive course with&nbsp;quizzes!'
+		},
+		{
+			href: 'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+			title: 'Templates',
+			description: 'Discover and deploy boilerplate example Next.js&nbsp;projects.'
+		},
+		{
+			href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+			title: 'Deploy',
+			description: 'Instantly deploy your Next.js site to a shareable URL with& nbsp; Vercel.'
+		}
+	];
 	return (
 		<div className={styles.grid}>
-			<a
-				href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2 className={[inter.className, styles.title]}>
-					Docs <span>-&gt;</span>
-				</h2>
-				<p className={[inter.className, styles.description]}>
-					Find in-depth information about Next.js features and&nbsp;API.
-				</p>
-			</a>
-
-			<a
-				href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2 className={[inter.className, styles.title]}>
-					Learn <span>-&gt;</span>
-				</h2>
-				<p className={[inter.className, styles.description]}>
-					Learn about Next.js in an interactive course with&nbsp;quizzes!
-				</p>
-			</a>
-
-			<a
-				href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2 className={[inter.className, styles.title]}>
-					Templates <span>-&gt;</span>
-				</h2>
-				<p className={[inter.className, styles.description]}>
-					Discover and deploy boilerplate example Next.js&nbsp;projects.
-				</p>
-			</a>
-
-			<a
-				href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2 className={[inter.className, styles.title]}>
-					Deploy <span>-&gt;</span>
-				</h2>
-				<p className={[inter.className, styles.description]}>
-					Instantly deploy your Next.js site to a shareable URL
-					with&nbsp;Vercel.
-				</p>
-			</a>
-		</div>
-	);
+			{ITEMS.map(item => {
+				return (
+					<a
+						key={item.href}
+						href={item.href}
+						className={styles.card}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={[inter.className, styles.title]}>
+							{item.title} <span>-&gt;</span>
+						</h2>
+						<p className={[inter.className, styles.description]}>{item.description}</p>
+					</a>
+				);
+			})}
+		</div >
+	)
 }
