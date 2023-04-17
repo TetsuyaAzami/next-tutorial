5fa3b6274bfb5493bebece7ed7d20bcef8828b37
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-18 07:11:49 +0900

    クリックイベントの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,28 @@ import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import Head from "next/head";
 import { Header } from "@/src/components/Header";
+import styles from "@/src/styles/index.module.css";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Index</title>
       </Head>
       <Header />
+      {/* <button onClick={() => alert("ボタンが押されました")}>ボタン</button> */}
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
