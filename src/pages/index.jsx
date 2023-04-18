e5479542cbc0ac2d8e1b71b681543651a4983d22
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-18 21:11:50 +0900

    useCallBack, useEffectの第二引数について演習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,15 +13,19 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
+
   return (
     <div>
       <Head>
