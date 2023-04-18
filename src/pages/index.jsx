58872e26a69149c71a1ded363c64b8756f37e421
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-19 07:09:43 +0900

    useStateで配列の演習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([1, 2, 3, 4]);
 
   const handleChange = useCallback((e) => {
     if (e.target.value.length > 3) {
@@ -33,6 +34,16 @@ export default function Home() {
     setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
+  const handleAdd = useCallback(() => {
+    return setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します。");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -51,6 +62,12 @@ export default function Home() {
         <button onClick={handleClick}>ボタン</button>
         <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
         <input type="text" value={text} onChange={handleChange} />
+        <button onClick={handleAdd}>追加</button>
+        <ul>
+          {array.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
       </div>
       <Main page="index" />
       <Footer />
