d28a2faacf4e472832395194e55a7a021c850469
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-19 06:14:53 +0900

    useStateで文字列と真偽値を演習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,12 +12,25 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 3) {
+      alert("3文字以上は入力できません。");
+    }
+    setText(e.target.value.trim());
+  });
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -33,8 +46,10 @@ export default function Home() {
       </Head>
       <Header />
       <div className={styles.container}>
-        {count}
+        {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+        <input type="text" value={text} onChange={handleChange} />
       </div>
       <Main page="index" />
       <Footer />
